12ff82e7f214a7d897dba330cb1817607d2a87ed
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2018-08-10 13:48:45 -0400

    Set up repo for vcfR Tutorial

--- install.R
+++ install.R
@@ -1,5 +1,2 @@
-install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages('leaflet')
+source("https://bioconductor.org/biocLite.R")
+biocLite("vcfR")
